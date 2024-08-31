5fc3bd92b79e71bc014a17009015b1a853879968
spaciousejar <jadhavvicky752@gmail.com>
2024-08-31 15:38:00 +0530

    adding commad menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,36 +1,25 @@
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-import { ModeToggle } from '@/components/ModeToggle'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+import { ModeToggle } from '@/components/mode-toggle'
+import { CommandMenu } from '@/components/command-menu'
 
 
 export default function Header() {
   return (
     <header>
-  
-      <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">
+      <nav className="flex items-center gap-4 text-sm lg:gap-6 justify-between py-1">
+        <Link href="/" className="text-2xl font-bold py-2">
           Period Cramp Soother
         </Link>
         <div className="space-x-4">
           <Link href="/dashboard">
             <Button variant="ghost">Dashboard</Button>
           </Link>
-          
           <Link href="/profile">
             <Button variant="ghost">Profile</Button>
           </Link>
+          <CommandMenu data={undefined}/>
           <ModeToggle/>
-          <Button variant="secondary">Logout</Button>
         </div>
       </nav>
     </header>
--- components/IntensityMonitor.tsx
+++ components/IntensityMonitor.tsx
@@ -1,10 +1,23 @@
-import { Progress } from '@/components/ui/progress'
+"use client"
+
+import { useState, useEffect } from "react"
+import { Slider } from "@/components/ui/slider"
+import { Progress } from "@/components/ui/progress"
 
 export default function IntensityMonitor({ level }: { level: number }) {
+  const intensity = (value: number) => {}; // Declare the 'intensity' variable as a function
   return (
-    <div>
+    <><div>
       <Progress value={level * 10} className="w-full" />
       <p className="mt-2 text-center">Current Intensity: {level}/10</p>
-    </div>
+    </div><div className="mb-6">
+        <h2 className="text-lg font-semibold mb-2 text-primary">Intensity Level</h2>
+        <Slider
+         defaultValue={[33]} 
+         max={100} 
+         step={1} />
+
+        <p className="mt-2 text-center text-primary">{intensity.toString()}%</p>
+      </div></>
   )
 }
