554d9ba661359095d90e3ed3c4cabce1a57dad27
codetil <dakshapatil960@gmail.com>
2025-01-14 21:15:29 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { Navbar } from '@/components/navbar'
-import Footer from '@/components/Footer'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ClerkProvider } from '@clerk/nextjs'
 import { Analytics } from "@vercel/analytics/react"
