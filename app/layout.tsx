41b8dd60498e937e8a30af7d2bdc1b056d1252b8
spaciousejar <jadhavvicky752@gmail.com>
2025-04-13 10:50:33 +0530

    make pwa display fullscreen and add meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body className={inter.className} suppressHydrationWarning>
         <ClerkProvider>
           <ThemeProvider
